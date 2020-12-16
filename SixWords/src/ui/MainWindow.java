f09973ebb476d68dd5d486f2164e0d4879b535c3
Zach Haymore <zjhaymor@ncsu.edu>
2020-12-16 10:25:35 -0500

    [UI] Added ActionListeners and the corresponding method to the buttons on the MainWindow.java

--- SixWords/src/ui/MainWindow.java
+++ SixWords/src/ui/MainWindow.java
@@ -7,6 +7,9 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -14,9 +17,17 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import model.manager.SixWordsManager;
+
 
 /**
  * The Main GUI component that will be used to launch the program and that will hold the buttons to 
@@ -24,7 +35,7 @@ import javax.swing.JPanel;
  * 
  * @author Zach Haymore
  */
-public class MainWindow extends JFrame{
+public class MainWindow extends JFrame implements ActionListener{
 	
 	/**Button to start creating new memoirs*/
 	private JButton btnStartNewMemoirs;
@@ -77,17 +88,65 @@ public class MainWindow extends JFrame{
 		btnStartNewMemoirs.setForeground(Color.WHITE);
 		btnLoadMemoirsFromFile.setForeground(Color.WHITE);
 		btnStartNewMemoirs.setOpaque(false);
+		
 		btnLoadMemoirsFromFile.setOpaque(false);
+		
+		btnStartNewMemoirs.addActionListener(this);
+		btnLoadMemoirsFromFile.addActionListener(this);
+		
 		btnPanel.add(btnStartNewMemoirs);
 		btnPanel.add(btnLoadMemoirsFromFile);
+		
+		
+	
+		
 		c.add(btnPanel, BorderLayout.SOUTH);
-		
-		
-		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
 	}
 	
+	
+	/**
+	 * Action performed method that gets called when an action is registered from the buttons. If 
+	 * the ActionEvent has the action command of "Create New Stories," then the program loads an empty
+	 * system, where new stories can be created, else then a FileChooser option loads and a file needs
+	 * to be selected with the memoirs that need to be loaded. The SixWordsManager is then loaded with 
+	 * the information from the file, and a new window will be launched. Closes the MainWindow after the 
+	 * action had been called and new Window has been loaded. 
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if  (e.getActionCommand().equals("Create New Stories")) {
+			
+			
+		
+		} else {
+			
+			try {
+				SixWordsManager.getInstance().resetSystem();
+				
+				JFileChooser chooser = new JFileChooser("./");
+				FileNameExtensionFilter filterExt = new FileNameExtensionFilter("Memoirs (txt)", "txt");
+				chooser.setFileFilter(filterExt);
+				chooser.setMultiSelectionEnabled(false);
+				int resultAction = chooser.showOpenDialog(this);
+				
+				if (resultAction == JFileChooser.APPROVE_OPTION) {
+					String fileName = chooser.getSelectedFile().getAbsolutePath();
+					
+					SixWordsManager.getInstance().loadMemoirsFromFile(fileName);
+				}
+			} catch (Exception f) {
+				JOptionPane.showMessageDialog(this, "Error opening file.", "Opening Error", JOptionPane.ERROR_MESSAGE);
+			}
+			
+		}
+		
+		 JComponent comp = (JComponent) e.getSource();
+		  Window win = SwingUtilities.getWindowAncestor(comp);
+		  win.dispose();
+	}
 	
 	/**
 	 * Starts the program by launching the MainWindow
@@ -96,6 +155,6 @@ public class MainWindow extends JFrame{
 	public static void main(String[] args) {
 		new MainWindow();
 	}
-	
+
 	
 }
