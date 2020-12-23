d6d3a484ef8ded549aafeb6d2a725fc57be19b33
Zach Haymore <zjhaymor@ncsu.edu>
2020-12-23 12:42:36 -0500

    [UI] Set up the valueChanged() for the ListSelectionListener interface that was implemented in order to access the information displayed on the list

--- SixWords/src/ui/HomeWindow.java
+++ SixWords/src/ui/HomeWindow.java
@@ -3,6 +3,7 @@
  */
 package ui;
 
+import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
@@ -11,8 +12,12 @@ import java.awt.event.ActionListener;
 import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JList;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
 import model.manager.SixWordsManager;
 import model.memoir.Memoir;
 
@@ -23,7 +28,7 @@ import model.memoir.Memoir;
  * 
  * @author Zach Haymore
  */
-public class HomeWindow extends JFrame implements ActionListener{
+public class HomeWindow extends JFrame implements ActionListener, ListSelectionListener {
 	
 	/**
 	 * A JList for the memoirs
@@ -34,6 +39,9 @@ public class HomeWindow extends JFrame implements ActionListener{
 	 * The array of Memoir objects in the system
 	 */
 	private Memoir[] memoirs;
+	
+	/**JPanel to hold the list of Memoirs and buttons*/
+	private JPanel pnlMemoir; 
 	
 	/**
 	 * UID needed for JFrame
@@ -49,16 +57,20 @@ public class HomeWindow extends JFrame implements ActionListener{
 		setLocation(200, 200);
 		setSize(600, 600);
 		
+		pnlMemoir = new JPanel();
 		listMemoirs = new JList<String>(); //data has type Object[]
 		listMemoirs.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
 		listMemoirs.setLayoutOrientation(JList.VERTICAL_WRAP);
 		listMemoirs.setVisibleRowCount(-1);
+		listMemoirs.addListSelectionListener(this);
+		
 		
 		JScrollPane listScroller = new JScrollPane();
 		listScroller.setPreferredSize(new Dimension(250, 80));
 		listScroller.setViewportView(listMemoirs);
 		
-		c.add(listScroller);
+		pnlMemoir.add(listScroller);
+		c.add(pnlMemoir, BorderLayout.CENTER);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
 		refreshMemoirs();
@@ -81,10 +93,20 @@ public class HomeWindow extends JFrame implements ActionListener{
 			memoirs = SixWordsManager.getInstance().getMemoirList();
 		}
 		
-		for(Memoir memoir : memoirs) {
+		for (Memoir memoir : memoirs) {
 			dlmOrders.addElement(memoir.getTitle());
 		}
 		listMemoirs.setModel(dlmOrders);
+	}
+	
+	/**
+	 * The valueChanged() that performs the action for the ListListener. 
+	 */
+	@Override
+	public void valueChanged(ListSelectionEvent e) {
+		if (e.getValueIsAdjusting()) {
+	      System.out.println(memoirs[e.getFirstIndex()]);
+	    }
 	}
 
 }
